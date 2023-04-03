febb185d37be51b0e14caf5fa99f1501b6d2cdbb
dyerlab <dyerlab@icloud.com>
2023-04-03 15:16:50 -0400

    making it an app

---
+++ PopGraph/PopGraphApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopGraphApp.swift
+//  PopGraph
+//
+//  Created by Rodney Dyer on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct PopGraphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- Sources/PopGraph/PopGraph.swift
+++
@@ -1,6 +0,0 @@
-public struct PopGraph {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
